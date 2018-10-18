65db8fe1c504daa152fe982d90e22dceda10d0c2
Mark Ewing <mewing@eastman.com>
2018-10-18 19:39:55 -0400

    initial commit of python code

---
+++ mgsub/mgsub.py
@@ -0,0 +1,28 @@
+import re
+from operator import itemgetter
+
+
+def mgsub(string, pattern=[], replacement=[]):
+    matches = []
+    for i in range(pattern.__len__()):
+        matches.extend(gregexpr(pattern[i], string, i))
+    matches.sort(key=itemgetter(2), reverse=True)
+    for i in range(matches.__len__()-1, -1, -1):
+        if any([filter_matches(matches[i], m) for m in matches[:i]]):
+            matches.pop(i)
+
+
+def gregexpr(pattern, string, i):
+    greg = re.finditer(pattern, string)
+    output = [[i, r.start(), r.group().__len__()] for r in greg]
+    for o in output:
+        o.append(o[1]+o[2])
+    return output
+
+
+def filter_matches(this, prev):
+    s = this[1]
+    e = this[3]
+    ps = prev[1]
+    pe = prev[3]
+    return (ps <= s & pe >= s) | (ps <= e & pe >= e)
