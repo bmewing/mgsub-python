2fb2c37e868a5507dfdf0cadc913bd98ebabfb52
Aditya Addepalli <30334566+Dyex719@users.noreply.github.com>
2018-10-19 07:39:43 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='mgsub',
+   version='1.0',
+   description='A safe, multiple, simultaneous string substitution function',
+   author='Mark Ewing',
+   packages=['mgsub'],  #same as name
+   install_requires=[], #no external packages as dependencies as of now
+   scripts=[], #no scripts as of now
+)
